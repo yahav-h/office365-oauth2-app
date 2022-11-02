7073aeea1ba1099927b12502f974f35f5bfc1634
yahav-h <yahavh@avanan.com>
2022-11-02 09:06:45 +0200

    spacing + add root_project variable

--- helpers.py
+++ helpers.py
@@ -6,19 +6,25 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from yaml import load, Loader
 
+proj_root = dirname(abspath(__file__))
+
+
 def loadmapping() -> dict:
     data = None
-    with open(join(getcwd(), 'resources', 'mapping.yml'), 'r') as out_stream:
+    with open(join(proj_root, 'resources', 'mapping.yml'), 'r') as out_stream:
         data = load(out_stream, Loader)
     return data
+
 
 def loadproperties() -> dict:
     data = None
-    with open(join(getcwd(), 'resources', 'properties.yml'), 'r') as out_stream:
+    with open(join(proj_root, 'resources', 'properties.yml'), 'r') as out_stream:
         data = load(out_stream, Loader)
     return data
 
+
 def getclientconfig() -> dict: return loadproperties()
+
 
 def getwebdriver():
     chrome_options = Options()
