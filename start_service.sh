2eb7611977dce6effd62b286a4155de86735ac43
yahav-h <yahavh@avanan.com>
2022-11-03 19:24:39 +0200

    fix start_service.sh - wrong filename

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source ./venv3/bin/activate
-./venv3/bin/python3 -m pip install --upgrade pip && ./venv3/bin/python3 -m pip install -r ./dependencies.txt
-sudo nohup ./venv3/bin/python3 ./o364_Service.py &
+./venv3/bin/python3 -m pip install --upgrade pip && ./venv3/bin/python3 -m pip install -r ./requirements.txt
+sudo nohup ./venv3/bin/python3 ./o365_Service.py &
