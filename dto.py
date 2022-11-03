8b4e26baf8b8f3cf94ccc62fd261d7ca06b930fc
yahav-h <yahavh@avanan.com>
2022-11-03 14:32:09 +0200

    update data transfer object

--- dto.py
+++ dto.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass, field, asdict
-from pickle import loads
 
 @dataclass
 class UserDataTransferObject:
     uid: int = field(init=True)
     user: str = field(init=True)
     token: bytes = field(init=True)
-    def decompress_token(self): return loads(self.token)
     def __repr__(self): return "<UserDTO %r>" % asdict(self)
