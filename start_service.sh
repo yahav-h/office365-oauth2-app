59b60e50f05918d0704371ada1afae795c8e7dd7
yahav-h <yahavh@avanan.com>
2022-11-08 16:21:17 +0200

    update script

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-source ./venv3/bin/activate
 ./venv3/bin/python3 -m pip install --upgrade pip && ./venv3/bin/python3 -m pip install -r ./dependencies.txt
 sudo nohup ./venv3/bin/python3 ./o365_Service.py &
