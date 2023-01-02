fdc80a6ae0fa5871ab83a969f1ef917764e4cc54
yahav-h <yahavh@avanan.com>
2023-01-02 15:21:00 +0200

    add incognito mode

--- helpers.py
+++ helpers.py
@@ -32,6 +32,7 @@ def getwebdriver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--incognito")
     driver_path = ChromeDriverManager().install()
     driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
     driver.delete_all_cookies()
